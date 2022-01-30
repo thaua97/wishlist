6ba18f6d615e377eef43180c8d38d3f2514fe618
Thauã Borges <thaua97@gmail.com>
2022-01-29 22:20:07 -0300

    :art: Fix responsive sizes

--- src/views/Product/index.vue
+++ src/views/Product/index.vue
@@ -1,10 +1,49 @@
 <template>
   <section class="mn-product">
     <article class="mn-product__description">
+      <span 
+        v-if="item.isFreeShipping"
+        class="mn-product__details--freeshipping"
+      >
+        <truck-icon fill="#7cdb67" /> &nbsp; Frete Grátis
+      </span>
       <h1 class="mn-product__title">
         {{ `${item.title} - ${item.description}` }} 
       </h1>
       <p>Ref.: {{item.sku}}</p>
+      <h1 class="mn-product__details--price">{{price}}</h1>
+      <ul class="mn-product__sizes">
+        <li
+          v-if="item.availableSizes.S > 0" 
+          class="mn-product__sizes--item"
+        > 
+          S
+        </li>
+        <li
+          v-if="item.availableSizes.M > 0" 
+          class="mn-product__sizes--item"
+        >
+          M
+        </li>
+        <li
+          v-if="item.availableSizes.G > 0" 
+          class="mn-product__sizes--item"
+        >
+          G
+        </li>
+        <li
+          v-if="item.availableSizes.GG > 0" 
+          class="mn-product__sizes--item"
+        >
+          GG
+        </li>
+        <li
+          v-if="item.availableSizes.GGG > 0" 
+          class="mn-product__sizes--item"
+        >
+          GGG
+        </li>
+      </ul>
     </article>
 
     <div class="mn-product__image">
@@ -14,47 +53,6 @@
       <img :src="item.image" :alt="item.title">
     </div>
 
-    <article class="mn-product__details">
-      <span 
-        v-if="item.isFreeShipping"
-        class="mn-product__details--freeshipping"
-      >
-        <truck-icon fill="#7cdb67" /> &nbsp; Frete Grátis
-      </span>
-      <h1 class="mn-product__details--price">{{price}}</h1>
-      <ul class="mn-product__sizes">
-        <li
-          v-if="item.availableSizes.S" 
-          class="mn-product__sizes--item"
-        > 
-          S
-        </li>
-        <li
-          v-if="item.availableSizes.M" 
-          class="mn-product__sizes--item"
-        >
-          M
-        </li>
-        <li
-          v-if="item.availableSizes.G" 
-          class="mn-product__sizes--item"
-        >
-          G
-        </li>
-        <li
-          v-if="item.availableSizes.GG" 
-          class="mn-product__sizes--item"
-        >
-          GG
-        </li>
-        <li
-          v-if="item.availableSizes.GGG" 
-          class="mn-product__sizes--item"
-        >
-          GGG
-        </li>
-      </ul>
-    </article>
   </section>
 </template>
 
